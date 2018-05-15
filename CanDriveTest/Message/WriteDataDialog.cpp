4beb30064042aad9dd1b927df5c8062ab4960939
tanchaung <tanchuang@seer-robotics.com>
2018-05-15 18:31:48 +0800

    xxx

---
+++ CanDriveTest/Message/WriteDataDialog.cpp
@@ -0,0 +1,58 @@
+﻿#include "WriteDataDialog.h"
+#include "ui_WriteDataDialog.h"
+#include <QtDebug>
+WriteDataDialog *WriteDataDialog::pWriteDataDialog = Q_NULLPTR;
+WriteDataDialog::WriteDataDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::WriteDataDialog)
+{
+    ui->setupUi(this);
+
+}
+
+WriteDataDialog *WriteDataDialog::init()
+{
+    if(!pWriteDataDialog)
+    {
+        pWriteDataDialog =new WriteDataDialog;
+    }
+    return pWriteDataDialog;
+}
+WriteDataDialog::~WriteDataDialog()
+{
+    delete ui;
+}
+
+void WriteDataDialog::on_pushButton_WriteOK_clicked()
+{
+    if(!ui->lineEdit_ConfigData->text().isEmpty())
+    {
+        QString valueText=ui->lineEdit_ConfigData->text();
+        float value = valueText.toFloat();
+//        qDebug()<<value <<"ccccccccccccccccccccccccccccccccvalueText.toFloat()";
+        QString indexStr= ui->label_value->text();
+        int indexValue = indexStr.toInt();
+       int valueBack= ControlCanDll::init()->WriteData(4,value,indexValue);
+       if( valueBack==0)
+       {
+           emit signalValue(indexValue, valueText);
+       }
+       if(valueBack==1)
+       {
+           emit signalValue(indexValue,QStringLiteral("写入的参数超过最大阈值 "));
+       }
+       if(valueBack==2)
+       {
+           emit signalValue(indexValue, QStringLiteral("写入的参数低于最小阈值 "));
+       }
+       close();
+    }
+
+}
+void WriteDataDialog::GetStatusValue(const QString &text , int value )
+{
+     setWindowTitle(QStringLiteral("写入窗"));
+     ui->label_attribute->setText(text);
+     ui->label_value ->setText(tr("%1").arg(value));
+     show();
+}
---
+++ CanDriveTest/Message/WriteDataDialog.h
@@ -0,0 +1,28 @@
+﻿#ifndef WRITEDATADIALOG_H
+#define WRITEDATADIALOG_H
+
+#include <QDialog>
+#include "ControlCanDll.h"
+namespace Ui {
+class WriteDataDialog;
+}
+
+class WriteDataDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    static WriteDataDialog *init();
+    explicit WriteDataDialog(QWidget *parent = 0);
+    ~WriteDataDialog();
+    void GetStatusValue(const QString &text , int value);
+private slots:
+    void on_pushButton_WriteOK_clicked();
+ signals:
+    void signalValue(int,QString);
+private:
+    static WriteDataDialog *pWriteDataDialog;
+    Ui::WriteDataDialog *ui;
+};
+
+#endif // WRITEDATADIALOG_H
